0ebf9dd40c9d82fac6041c49d0c6c34feaa3ee92
CristianBulat <cristy.bulat@gmail.com>
2022-08-09 10:05:35 +0300

    Chat Window (part 1) + HW

---
+++ Chat Window/chat.js
@@ -0,0 +1,28 @@
+const chatWindow = document.getElementById('chat')
+let messages = []
+
+function initChat() {
+    let chatHidden = localStorage.getItem('chatHidden')
+    if(!chatHidden){
+        openChat()
+    }
+}
+
+function openChat() {
+  chatWindow.classList.remove('hidden')
+  chatHidden = false
+  localStorage.removeItem('chatHidden')
+}
+
+function closeChat() {
+    chatWindow.classList.add('hidden')
+    localStorage.setItem('chatHidden', true)
+}
+
+
+function sendMessage() {
+  const boxvalue = document.getElementsByClassName('text-input');
+  messages.push(boxvalue[0].value);
+  console.log(messages);
+  boxvalue[0].value = '';
+}
